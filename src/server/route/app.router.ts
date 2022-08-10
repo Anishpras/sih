448bc84269d49bdd1e9a9c16262b2df14392fd95
anishpras <anishprashun118@gmail.com>
2022-08-10 17:31:05 +0530

    Client roter added

--- src/server/route/app.router.ts
+++ src/server/route/app.router.ts
@@ -1,6 +1,9 @@
+import { clientRouter } from "./client.route";
 import { createRouter } from "../createRouter";
 import { arbitratorRouter } from "./arbitrators.route";
 
-export const appRouter = createRouter().merge("arbitrators.", arbitratorRouter);
+export const appRouter = createRouter()
+  .merge("arbitrators.", arbitratorRouter)
+  .merge("clients.", clientRouter);
 
 export type AppRouter = typeof appRouter;
