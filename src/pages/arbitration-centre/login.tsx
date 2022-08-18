0baee77b4f39c31cc3f6b6ff4a94b25ea2eed753
kumard3 <kumardeepanshu157200@gmail.com>
2022-08-18 17:34:32 +0530

    login screen added

--- src/pages/arbitration-centre/login.tsx
+++ src/pages/arbitration-centre/login.tsx
@@ -2,6 +2,8 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 
 import { useForm } from "react-hook-form";
+import { Button } from "../../components/login/Button";
+import { Input } from "../../components/login/Input";
 import { useArbitrationCentreContext } from "../../context/arbitrationCentre.context";
 import { trpc } from "../../utils/trpc";
 
@@ -60,22 +62,69 @@ const Login = () => {
     router.push("/arbitration-centre");
   }
   return (
-    <div className="bg-black text-white w-full min-h-screen ">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className="text-black"
-          {...register("arbitrationCentreId")}
-          placeholder="arbitrationCentreId"
+    <div className="min-h-screen w-full bg-primary text-white ">
+      <div className="p-4">
+        <img
+          src="/minister-of-finance-logo.svg"
+          alt="minister-of-finance-logo"
+          loading="lazy"
+          width="250"
+          height="150"
         />
-        <input
-          className="text-black"
-          {...register("password")}
-          placeholder="Last name"
-        />
-        <button type="submit">Submit</button>
-      </form>
+      </div>
+      <div className="relative top-20 flex h-full w-full flex-col items-center justify-center ">
+        <div>
+          <img
+            alt="header-logo"
+            src="/header-logo.svg"
+            height="200"
+            width="300"
+            loading="lazy"
+          />
+        </div>
+        <div>
+          <h1 className="font-bol text-xl">ARBITRATOR LOGIN</h1>
+        </div>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="ite container mx-auto flex w-full flex-col content-center items-center justify-center justify-self-center object-center "
+        >
+          <Input
+            type="text"
+            register={register}
+            placeholder="Enter your Arbitration Centre Id"
+            registerName={"arbitrationCentreId"}
+          />
+          <Input
+            type="text"
+            register={register}
+            placeholder="Enter your password"
+            registerName={"password"}
+          />
+          <Button type="submit">Login</Button>
+        </form>
+      </div>
     </div>
   );
 };
+//   return (
+//     <div className="bg-black text-white w-full min-h-screen ">
+//       <form onSubmit={handleSubmit(onSubmit)}>
+//         <input
+//           className="text-black"
+//           {...register("arbitrationCentreId")}
+//           placeholder="arbitrationCentreId"
+//         />
+//         <input
+//           className="text-black"
+//           {...register("password")}
+//           placeholder="Last name"
+//         />
+//         <button type="submit">Submit</button>
+//       </form>
+//     </div>
+//   );
+// };
 
 export default Login;
--- src/pages/arbitrator/login.tsx
+++ src/pages/arbitrator/login.tsx
@@ -2,6 +2,8 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { trpc } from "../../utils/trpc";
 import { useForm } from "react-hook-form";
+import { Input } from "../../components/login/Input";
+import { Button } from "../../components/login/Button";
 
 interface FormData {
   registrationId: string;
@@ -48,22 +50,50 @@ const ArbitratorLogin = () => {
       />
     );
   }
-
   return (
-    <div className="bg-black text-white w-full min-h-screen ">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className="text-black"
-          {...register("registrationId")}
-          placeholder="First name"
+    <div className="min-h-screen w-full bg-primary text-white ">
+      <div className="p-4">
+        <img
+          src="/minister-of-finance-logo.svg"
+          alt="minister-of-finance-logo"
+          loading="lazy"
+          width="250"
+          height="150"
         />
-        <input
-          className="text-black"
-          {...register("password")}
-          placeholder="Last name"
-        />
-        <button type="submit">Submit</button>
-      </form>
+      </div>
+      <div className="relative top-20 flex h-full w-full flex-col items-center justify-center ">
+        <div>
+          <img
+            alt="header-logo"
+            src="/header-logo.svg"
+            height="200"
+            width="300"
+            loading="lazy"
+          />
+        </div>
+        <div>
+          <h1 className="font-bol text-xl">ARBITRATOR LOGIN</h1>
+        </div>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="ite container mx-auto flex w-full flex-col content-center items-center justify-center justify-self-center object-center "
+        >
+          <Input
+            type="text"
+            register={register}
+            placeholder="Enter your Registration Id"
+            registerName={"registrationId"}
+          />
+          <Input
+            type="text"
+            register={register}
+            placeholder="Enter your password"
+            registerName={"password"}
+          />
+          <Button type="submit">Login</Button>
+        </form>
+      </div>
     </div>
   );
 };
