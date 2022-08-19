f3ea9506e60a0070b9dce87514553e5d59f0dd88
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-08-19 13:43:48 +0530

    first

--- schema/adminSchema.schema.ts
+++ schema/adminSchema.schema.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { verifyAdminSchema, VerifyAdminSchema } from './arbitrationCentreSchema.schema';
 
 export const createAdminSchema = z.object({
   name: z.string(),
@@ -12,5 +13,10 @@ export const loginAdminSchema = z.object({
   password: z.string(),
 });
 
+export const verifyArbitratorSchema = z.object({
+  arbitratorId: z.string(),
+})
+
 export type CreateAdminSchema = z.TypeOf<typeof createAdminSchema>;
 export type LoginAdminSchema = z.TypeOf<typeof loginAdminSchema>;
+export type VerifyArbitratorSchema = z.TypeOf<typeof verifyArbitratorSchema>;
