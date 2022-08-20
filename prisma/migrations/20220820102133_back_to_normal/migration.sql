1bff28ac1f38f2fd0e36e6ccdb1284692e49f6b1
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-08-20 16:26:01 +0530

    Client Registration Through Arbitrator

---
+++ prisma/migrations/20220820102133_back_to_normal/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Made the column `caseId` on table `Case` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Case" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "caseId" TEXT NOT NULL,
+    "award" TEXT,
+    "arbitratorId" TEXT,
+    CONSTRAINT "Case_arbitratorId_fkey" FOREIGN KEY ("arbitratorId") REFERENCES "Arbitrator" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Case" ("arbitratorId", "award", "caseId", "createdAt", "description", "id", "name", "updatedAt") SELECT "arbitratorId", "award", "caseId", "createdAt", "description", "id", "name", "updatedAt" FROM "Case";
+DROP TABLE "Case";
+ALTER TABLE "new_Case" RENAME TO "Case";
+CREATE UNIQUE INDEX "Case_caseId_key" ON "Case"("caseId");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
