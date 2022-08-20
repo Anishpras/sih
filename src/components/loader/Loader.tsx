f95bf34d7520a4b8866f783100cfba792f22ddf0
Kumar Abhishek <59351747+krabhi4@users.noreply.github.com>
2022-08-20 09:30:02 +0530

    spinner added

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export const Loader = () => {
   return (
-<h1>Loading...</h1>
-  )
-}
+    <div className="fixed top-0 left-0 right-0 bottom-0 z-50 flex h-screen w-full items-center justify-center overflow-hidden bg-primary opacity-75">
+      <div
+        className="spinner-border inline-block h-8 w-8 animate-spin rounded-full"
+        role="status"
+      >
+        <span className="visually-hidden">
+          <svg
+            className="-inline-block h-8 w-8 animate-spin rounded-full"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </span>
+      </div>
+    </div>
+  );
+};
