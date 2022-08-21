938e02acac1ab47925e005f4ab97380434a91cd8
anishpras <anishprashun118@gmail.com>
2022-08-21 11:17:53 +0530

    cases showing

--- src/pages/arbitrator/cases/[case].tsx
+++ src/pages/arbitrator/cases/[case].tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { trpc } from "../../../utils/trpc";
 const SingleCase = () => {
-const [addCaseID,setCaseId] = useState("");
-  const [name, setName] = useState("")
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("123456")
-  const { mutate, error:createMutationError } = trpc.useMutation(
+  const [addCaseID, setCaseId] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("123456");
+  const { mutate, error: createMutationError } = trpc.useMutation(
     ["arbitrators.create-client"],
     {
       onError: (error) => {
@@ -14,11 +14,11 @@ const [addCaseID,setCaseId] = useState("");
       },
       onSuccess: () => {
         // router.push("/client/login");
-        console.log("success")
+        console.log("success");
       },
     }
   );
-  
+
   const router = useRouter();
   const { case: caseId } = router.query;
 
@@ -26,29 +26,48 @@ const [addCaseID,setCaseId] = useState("");
     "arbitrators.get-single-case",
     { caseId: caseId?.toString() },
   ]);
-  
 
   //Handle form submit
-  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleSubmit = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
     mutate({
-      name, username, password,
+      name,
+      username,
+      password,
       caseId: caseId?.toString(),
     });
+  };
 
-  }
-
-
-
-  console.log(data?.caseDetail?.caseId);
+  console.log(data);
   return (
     <div>
       <h1>Single Case</h1>
-    <h1>Add Your Client</h1>
-      <input type="text" value={name} placeholder="Client Name" onChange={e => setName(e.target.value)}/>
-      <input type="text" value={username} placeholder="Client Username" onChange={e => setUsername(e.target.value)} />
-      <input type="password" value={password} placeholder="Client Password" onChange={e => setPassword(e.target.value)} />
-      <button type="submit" onClick={(e) => handleSubmit(e)}>Add Client</button>
+      <h1>Add Your Client</h1>
+      <input
+        type="text"
+        value={name}
+        placeholder="Client Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        value={username}
+        placeholder="Client Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="Client Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit" onClick={(e) => handleSubmit(e)}>
+        Add Client
+      </button>
+
+      <h1>Add </h1>
     </div>
   );
 };
