1d723cc1b89f34669306d82087ab14495ddf7e15
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-08-25 00:43:02 +0530

    editor added (#33)
    Co-authored-by: kumard3 <kumardeepanshu157200@gmail.com>
    Co-authored-by: Kumar Deepanshu <62591080+kumard3@users.noreply.github.com>

--- src/components/login/Button.tsx
+++ src/components/login/Button.tsx
@@ -6,9 +6,9 @@ interface buttonProps {
 }
 
 export const ButtonStyle =
-  "min-w-[220px] max-w-sm hover:bg-hoverWhite customShadow font-Montserrat font-semibold my-1 rounded-md bg-white   py-2 uppercase  text-black   placeholder:text-gray-800 md:min-w-[330px]  ";
-export const CommonButton = 
-" rounded-md bg-white px-5  py-2  text-black shadow-md  placeholder:text-gray-800 ";
+  "min-w-[220px] max-w-sm hover:bg-hoverWhite customShadow font-Montserrat font-semibold my-1 rounded-md bg-white   py-2   text-black   placeholder:text-gray-800 md:min-w-[330px]  ";
+export const CommonButton =
+  " rounded-md bg-white px-5  py-2  text-black shadow-md  placeholder:text-gray-800 ";
 
 export const Button = (props: buttonProps) => {
   return (
